8b1e4744d57df51272ee5495443a3e5b5ffff271
sou-xettri <rakeshchhetri420@gmail.com>
2024-04-04 08:50:28 +0545

    Add react-router-dom dependency and update tailwind.config.js and App.tsx to use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
+function App() {
   return (
-    <>
-     
-    </>
-  )
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/">
+            <Route index element={"Home"} />
+            <Route path="about" element={"About"} />
+            <Route path="projects" element={"Projects"} />
+            <Route path="contact" element={"Contact"} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
-export default App
+export default App;
