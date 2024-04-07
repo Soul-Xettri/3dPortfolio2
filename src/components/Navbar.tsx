ca36ae9dab71dc038a58b05919522141e9f496ff
sou-xettri <rakeshchhetri420@gmail.com>
2024-04-07 07:44:44 +0545

    Update Navbar logo from "AH" to "RC"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         to="/"
         className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
-        <p className="blue-gradient_text">AH</p>
+        <p className="blue-gradient_text">RC</p>
       </NavLink>
       <NavLink to="/about" className={({isActive})=>isActive?'text-blue-500':'text-black'}>
         About
